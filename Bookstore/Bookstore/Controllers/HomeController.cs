bb6566b60ea3f8e0bfca2cb7d18fe88769f3ebbb
Vini@Vinicius <vosiqueira@gmail.com>
2024-10-30 14:07:00 -0300

    adicionando estrutura basica do projeto

--- Bookstore/Bookstore/Controllers/HomeController.cs
+++ Bookstore/Bookstore/Controllers/HomeController.cs
@@ -1,32 +1,43 @@
 using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+
 using Microsoft.AspNetCore.Mvc;
+
 using System.Diagnostics;
 
 namespace Bookstore.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+{
+
+    public class HomeController : Controller
+
+    {
 
         public IActionResult Index()
+
         {
+
             return View();
+
         }
 
         public IActionResult Privacy()
+
         {
+
             return View();
+
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
+
         {
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
         }
+
     }
+
 }
+
